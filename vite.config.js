ebb660df95eeac10d4c98f85c781c28478de1fb2
Leonardo Contreras Roa <lcontrerasroa@gmail.com>
2026-03-11 13:57:16 +0100

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // ⚠️ Remplacez "vowel-space-tracker" par le nom exact de votre repo GitHub
-  base: '/vowel-space-tracker/',
+  base: '/vowel-tracker/',
 })
